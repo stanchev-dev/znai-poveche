006126c963208471a377079780be58237563856c
Martin Stanchev <92334759+stanchev-dev@users.noreply.github.com>
2026-02-13 00:11:13 +0200

    Add listings management table in Django admin

--- apps/marketplace/admin.py
+++ apps/marketplace/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "subject",
+        "owner",
+        "price_per_hour",
+        "online_only",
+        "vip_until",
+        "created_at",
+    )
+    list_filter = ("online_only", "subject", "created_at", "vip_until")
+    search_fields = (
+        "owner__username",
+        "owner__email",
+        "subject__name",
+        "description",
+    )
+    ordering = ("-created_at",)
